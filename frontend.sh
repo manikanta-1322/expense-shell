684392f3d4a5cae84aebe81f2157bb1778a3a381
bharath-gaveni <bharathgaveni@gmail.com>
2024-04-08 10:45:56 +0530

    Checking

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,13 @@
-log_file = "/tmp/expense.log"
-color = "\e[33m"
+dnf install nginx -y &>>/tmp/expense.log
 
-dnf install nginx -y &>>log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
-cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
-rm -rf /usr/share/nginx/html/* &>>log_file
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+cd /usr/share/nginx/html &>>/tmp/expense.log
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 
-cd /usr/share/nginx/html &>>log_file
-unzip /tmp/frontend.zip &>>log_file
-
-systemctl enable nginx &>>log_file
-systemctl start nginx &>>log_file
+systemctl enable nginx &>>/tmp/expense.log
+systemctl start nginx &>>/tmp/expense.log
