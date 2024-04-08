5cb131e774f0307865bf424b036963825db913b2
bharath-gaveni <bharathgaveni@gmail.com>
2024-04-08 11:10:56 +0530

    Checking

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,22 @@
 log_file="/tmp/expense.log"
+color="\e[36m"
 
-
-echo -e "\e[36m Installing ngInx \e[0m"
+echo -e "${color} Installing ngInx \e[0m"
 dnf install nginx -y &>>$log_file
 
-echo -e "\e[36m Installing ngInx \e[0m"
+echo -e "${color} Installing ngInx \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
+echo -e "${color} Installing ngInx \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
 
+echo -e "${color} Installing ngInx \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 
+echo -e "${color} Installing ngInx \e[0m"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
 
+echo -e "${color} Installing ngInx \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl start nginx &>>$log_file
