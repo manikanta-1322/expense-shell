53f14feaf7c99c63b73dd323e900a148a773cf32
bharath-gaveni <bharathgaveni@gmail.com>
2024-04-08 11:40:20 +0530

    Checking

--- backend.sh
+++ backend.sh
@@ -1,24 +1,41 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+log_file="/tmp/expense.log"
+color="\e[33m"
 
-dnf install nodejs -y
 
-cp backend.service /etc/systemd/system/backend.service
+echo -e "${color} Disable nodejs default version \e[0m"
+dnf module disable nodejs -y &>>$log_file
 
-useradd expense
+echo -e "${color} Download node js 18 version \e[0m"
+dnf module enable nodejs:18 -y &>>$log_file
 
-mkdir /app
+echo -e "${color} install node js \e[0m"
+dnf install nodejs -y &>>$log_file
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
+echo -e "${color} Download backend application file \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
-cd /app
-npm install
+echo -e "${color} Add application user \e[0m"
+useradd expense &>>$log_file
 
-dnf install mysql -y
-mysql -h mysql-dev.manireddy.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo -e "${color} Create application directory \e[0m"
+mkdir /app &>>$log_file
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+echo -e "${color} Download Application content \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
+
+echo -e "${color} Extract application content \e[0m"
+cd /app &>>$log_file
+unzip /tmp/backend.zip &>>$log_file
+
+echo -e "${color} Install nodejs dependencies \e[0m"
+cd /app &>>$log_file
+npm install &>>$log_file
+
+echo -e "${color} Install mySql to load schema \e[0m"
+dnf install mysql -y &>>$log_file
+mysql -h mysql-dev.manireddy.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+
+echo -e "${color} Starting backend service \e[0m"
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,19 @@
-dnf module disable mysql -y
+log_file="/tmp/expense.log"
+color="\e[33m"
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
 
-dnf install mysql-community-server -y
+echo -e "${color} Disable MySql default version \e[0m"
+dnf module disable mysql -y &>>$log_file
 
-systemctl enable mysqld
-systemctl start mysqld
+echo -e "${color} Copy MySql Repo file \e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo -e "${color} Install MySql Server \e[0m"
+dnf install mysql-community-server -y &>>$log_file
+
+echo -e "${color} Start MySql Server \e[0m"
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
+
+echo -e "${color} Set MySql Password \e[0m"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
