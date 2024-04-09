cf18360a3f7ff09586c2a43b500b726c694f2e7e
bharath-gaveni <bharathgaveni@gmail.com>
2024-04-09 14:24:56 +0530

    Checking

--- backend.sh
+++ backend.sh
@@ -3,6 +3,14 @@ color="\e[33m"
 
 MYSQL_ROOT_PASSWORD=$1
 
+status_check() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
+}
+
 if [ -z "$1" ]; then
   echo Password is Missing
   exit
@@ -10,106 +18,58 @@ fi
 
 echo -e "${color} Disable nodejs default version \e[0m"
 dnf module disable nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 echo -e "${color} Download node js 18 version \e[0m"
 dnf module enable nodejs:18 -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 echo -e "${color} install node js \e[0m"
 dnf install nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 echo -e "${color} Download backend application file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 id expense &>>$log_file
 if [ $? -ne 0 ]; then
   echo -e "${color} Add application user \e[0m"
   useradd expense &>>$log_file
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
-  else
-    echo -e "\e[31m FAILURE \e[0m"
-  fi
+  status_check
 fi
 
 if [ ! -d /app]; then
   echo -e "${color} Create application directory \e[0m"
   mkdir /app &>>$log_file
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
-  else
-    echo -e "\e[31m FAILURE \e[0m"
-  fi
+  status_check
 fi
 
 echo -e "${color} Delete old content \e[0m"
 rm -rf /app/* $>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 echo -e "${color} Download Application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 echo -e "${color} Extract application content \e[0m"
 cd /app &>>$log_file
 unzip /tmp/backend.zip &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 echo -e "${color} Install nodejs dependencies \e[0m"
 cd /app &>>$log_file
 npm install &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 echo -e "${color} load schema \e[0m"
 dnf install mysql -y &>>$log_file
 mysql -h mysql-dev.manireddy.online -uroot -p${MYSQL_ROOT_PASSWORD} < /app/schema/backend.sql &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 echo -e "${color} Starting backend service \e[0m"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
