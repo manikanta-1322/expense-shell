4f2e9350fef811092a5cc17150a26d13593d6804
bharath-gaveni <bharathgaveni@gmail.com>
2024-04-09 15:05:03 +0530

    Checking

--- backend.sh
+++ backend.sh
@@ -1,15 +1,6 @@
-log_file="/tmp/expense.log"
-color="\e[33m"
+source common.sh
 
 MYSQL_ROOT_PASSWORD=$1
-
-status_check() {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
-  else
-    echo -e "\e[31m FAILURE \e[0m"
-  fi
-}
 
 if [ -z "$1" ]; then
   echo Password is Missing
--- mysql.sh
+++ mysql.sh
@@ -1,47 +1,32 @@
-log_file="/tmp/expense.log"
-color="\e[33m"
+source common.sh
 
+MYSQL_ROOT_PASSWORD=$1
+
+if [ -z "$1" ]; then
+  echo Password is Missing
+  exit
+fi
 
 echo -e "${color} Disable MySql default version \e[0m"
 dnf module disable mysql -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 echo -e "${color} Copy MySql Repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 
 echo -e "${color} Install MySql Server \e[0m"
 dnf install mysql-community-server -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 
 echo -e "${color} Start MySql Server \e[0m"
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+status_check
 
 
 echo -e "${color} Set MySql Password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-  echo -e "\e[31m FAILURE \e[0m"
-fi
+mysql_secure_installation --set-root-pass ${MYSQL_ROOT_PASSWORD} &>>$log_file
+status_check
